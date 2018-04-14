db5f3d934d5509caa3145abe4fca255f68dc820b
CarlMichel Fevrius <cfevrius@student06.cse.nd.edu>
2018-04-13 20:26:12 -0400

    Adding gsc.cpp

---
+++ gsc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* PROGRAM_NAME = NULL;
+
+/* Functions */
+void usage(int status){
+    fprintf(stderr, "Usage: %s FILES...\n", PROGRAM_NAME);
+    exit(status);
+}
+
+/* Main Execution */
+int main(int argc, char* argv[]) {
+    
+    int argind = 1;
+    
+    /* Parse command line arguments */
+    PROGRAM_NAME = argv[0];
+    while(argind < argc && strlen(argv[argind]) > 1 && argv[argind][0] == '-'){
+        char * arg = argv[argind++];
+        switch(arg[1]){
+            case 'h':
+                usage(0);
+                break;
+            default:
+                usage(1);
+                break;
+      }
+  }
+  
+  return EXIT_SUCCESS;
+}
